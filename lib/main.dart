8c8b7d14806b37d9ba656413025e5a338c576753
BatStateU <mikkimamaradlo@gmail.com>
2025-06-19 10:20:49 +0800

    basketball Drills Workout

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,142 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text('Basketball Drills Workout'),
-        backgroundColor: Colors.grey[700],
+        title: Text('Basketball Drils Workout'),
+        backgroundColor: Colors.blue[100],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-          children: [
-            Text('Hapon 1'),
-            Text('Che-Che 2'),
-            Text('Beb 3'),
-          ],
-        ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(
+                color: Colors.blue,
+                width: 5,
+              ),
+            ),
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Name:',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Mikki',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Age:',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  '23 years old',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(5, 4, 3, 2),
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Gender:',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Male',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,  // changed to red
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     ),
   ));
 }
-
-
-
