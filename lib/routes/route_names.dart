6556dae85ed2458493f9cc16de0b19c786aa738a
CutieMiks <148317755+MikkiMamaradlo@users.noreply.github.com>
2025-07-22 14:16:49 +0800

    Delete lib directory

--- lib/routes/route_names.dart
+++
@@ -1,8 +0,0 @@
-class RouteNames {
-  static const String home = '/';
-  static const String routine = '/routine';
-  static const String drills = '/drills';
-  static const String settings = '/settings';
-  static const String workout = '/workout';
-  static const String drillDetail = '/drill-detail';
-}
